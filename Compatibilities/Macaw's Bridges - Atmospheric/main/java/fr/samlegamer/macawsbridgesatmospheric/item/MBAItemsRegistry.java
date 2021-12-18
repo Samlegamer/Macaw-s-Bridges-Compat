a458260ed1f616f4ce026b9b5e4e939701e7998e
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2021-12-18 19:55:09 +0100

    1.0

---
+++ Compatibilities/Macaw's Bridges - Atmospheric/main/java/fr/samlegamer/macawsbridgesatmospheric/item/MBAItemsRegistry.java
@@ -0,0 +1,11 @@
+package fr.samlegamer.macawsbridgesatmospheric.item;
+
+import fr.samlegamer.macawsbridgesatmospheric.MacawsBridgesAtmospheric;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class MBAItemsRegistry
+{
+	public static final DeferredRegister<Item> ITEMS_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MacawsBridgesAtmospheric.MODID);
+}
