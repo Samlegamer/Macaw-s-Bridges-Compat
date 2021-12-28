3ed03f576682f09e4a4651c73b6c3c2fe747121a
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2021-12-28 12:41:47 +0100

    1.0-1.18.1

---
+++ Compatibilities/Macaw's Bridges - Biome O' Plenty/main/java/fr/samlegamer/macawsbridgesbop/item/MBBOPItemsRegistry.java
@@ -0,0 +1,9 @@
+package fr.samlegamer.macawsbridgesbop.item;
+
+import fr.samlegamer.macawsbridgesbop.MacawsBridgesBOP;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class MBBOPItemsRegistry
+{public static final DeferredRegister<Item> ITEMS_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MacawsBridgesBOP.MODID);}
