bf26e575d591837165b42ffbc1a914efdc839844
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2022-01-18 00:06:52 +0100

    1.1 release

---
+++ Macaws Mods - JSON API/src/fr/samlegamer/code/MacawsBridgesCode.java
@@ -0,0 +1,330 @@
+package fr.samlegamer.code;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MacawsBridgesCode
+{
+	public static void init(String location)
+	{
+		File folderCode = new File(location + "code/");
+		File folderRenderType = new File(location + "code/RenderType/");
+		File folderBlocks = new File(location + "code/Blocks/");
+
+		if(!folderCode.exists() || !folderRenderType.exists() || !folderBlocks.exists())
+		{
+			folderCode.mkdir();
+			folderRenderType.mkdir();
+			folderBlocks.mkdir();
+		}
+	}
+	
+	public static class RenderType
+	{
+		public static void BridgeLog(String Location, ArrayList<String> Material, String ClassMod)
+		{
+			File file = new File(Location + "/code/RenderType/" + "RenderTypeBridges" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("RenderTypeLookup.setRenderLayer("+ClassMod+"."+i+"_log_bridge_middle.get(), RenderType.cutout());");
+						buffer.newLine();
+					}
+					
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		public static void RopeBridge(String Location, ArrayList<String> Material, String ClassMod)
+		{
+			File file = new File(Location + "/code/RenderType/" + "RenderTypeRopesBridges" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("		RenderTypeLookup.setRenderLayer("+ClassMod+".rope_"+i+"_bridge.get(), RenderType.cutout());");
+						buffer.newLine();
+					}
+					
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		public static void PierBridgeWood(String Location, ArrayList<String> Material, String ClassMod)
+		{
+			File file = new File(Location + "/code/RenderType/" + "RenderTypePierBridges" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("RenderTypeLookup.setRenderLayer("+ClassMod+"."+i+"_bridge_pier.get(), RenderType.cutout());");
+						buffer.newLine();
+					}
+					
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
+		public static void BridgeStairsLog(String Location, ArrayList<String> Material, String ClassMod)
+		{
+			File file = new File(Location + "/code/RenderType/" + "RenderTypeStairsBridges" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("RenderTypeLookup.setRenderLayer("+ClassMod+"."+i+"_log_bridge_stair.get(), RenderType.cutout());");
+						buffer.newLine();
+					}
+					
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
+		public static void BridgeRail(String Location, ArrayList<String> Material, String ClassMod)
+		{
+			File file = new File(Location + "/code/RenderType/" + "RenderTypeRailsBridges" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("RenderTypeLookup.setRenderLayer("+ClassMod+"."+i+"_rail_bridge.get(), RenderType.cutout());");
+						buffer.newLine();
+					}
+					
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	public static class BlockRegistry
+	{
+		public static void BridgeLog(String Location, ArrayList<String> Material)
+		{
+			File file = new File(Location + "/code/Blocks/" + "BlocksBridges" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("public static final RegistryObject<Block> "+i+"_log_bridge_middle = createBlock(\""+i+"_log_bridge_middle\", () -> new Log_Bridge(AbstractBlock.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));");
+						buffer.newLine();
+					}
+					
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		public static void RopeBridge(String Location, ArrayList<String> Material)
+		{
+			File file = new File(Location + "/code/Blocks/" + "BlocksRopeBridges" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("public static final RegistryObject<Block> rope_"+i+"_bridge = createBlock(\"rope_"+i+"_bridge\", () -> new Rope_Bridge(AbstractBlock.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));");
+						buffer.newLine();
+					}
+					
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		public static void PierBridgeWood(String Location, ArrayList<String> Material)
+		{
+			File file = new File(Location + "/code/Blocks/" + "BlocksPierBridges" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("public static final RegistryObject<Block> "+i+"_bridge_pier = createBlock(\""+i+"_bridge_pier\", () -> new Support_Pillar(AbstractBlock.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));");
+						buffer.newLine();
+					}
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
+		public static void BridgeStairsLog(String Location, ArrayList<String> Material)
+		{
+			File file = new File(Location + "/code/Blocks/" + "BlocksBridgesStairs" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("public static final RegistryObject<Block> "+i+"_log_bridge_stair = createBlock(\""+i+"_log_bridge_stair\", () -> new Iron_Stair(AbstractBlock.Properties.of(Material.WOOD).strength(0.5F, 2.5F).sound(SoundType.WOOD)));");
+						buffer.newLine();
+					}
+					
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+
+		public static void BridgeRail(String Location, ArrayList<String> Material)
+		{
+			File file = new File(Location + "/code/Blocks/" + "BlocksBridgesRails" + ".java");
+
+			if(!file.exists())
+			{
+				try
+				{
+					FileWriter writer = new FileWriter(file);
+					BufferedWriter buffer = new BufferedWriter(writer);
+					
+					for(String i : Material)
+					{
+						buffer.write("public static final RegistryObject<Block> "+i+"_rail_bridge = createBlock(\""+i+"_rail_bridge\", () -> new Rail_Bridge(AbstractBlock.Properties.of(Material.WOOD).noOcclusion().strength(0.5F, 2.5F).sound(SoundType.WOOD)));");
+						buffer.newLine();
+					}
+					
+					
+					buffer.close();
+					writer.close();
+					file.createNewFile();
+					System.out.println("Le fichier " + file + " Vient d'être générer à l'emplacement : " + Location);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+}
