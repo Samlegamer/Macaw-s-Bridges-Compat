39120060035dc5b960b6fded8fae16eb9dfa0090
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2022-02-19 01:36:26 +0100

    Add files via upload

---
+++ Compatibilities/Macaw's Bridges - Bayou Blue/src/main/java/fr/samlegamer/mcwbridgesbayoublues/MBBBBlocks.java
@@ -0,0 +1,36 @@
+package fr.samlegamer.mcwbridgesbayoublues;
+
+import com.google.common.base.Supplier;
+import com.mcwbridges.kikoz.objects.Iron_Stair;
+import com.mcwbridges.kikoz.objects.Log_Bridge;
+import com.mcwbridges.kikoz.objects.Rail_Bridge;
+import com.mcwbridges.kikoz.objects.Rope_Bridge;
+import com.mcwbridges.kikoz.objects.Support_Pillar;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.Block;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class MBBBBlocks
+{
+	public static final DeferredRegister<Block> BLOCKS_REGISTRY = DeferredRegister.create(ForgeRegistries.BLOCKS, McwBridgesBayouBlues.MODID);
+	
+	public static final RegistryObject<Block> cypress_log_bridge_middle = createBlock("cypress_log_bridge_middle", () -> new Log_Bridge(AbstractBlock.Properties.of(Material.WOOD).noOcclusion().strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+	public static final RegistryObject<Block> cypress_rail_bridge = createBlock("cypress_rail_bridge", () -> new Rail_Bridge(AbstractBlock.Properties.of(Material.WOOD).noOcclusion().strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+	public static final RegistryObject<Block> cypress_log_bridge_stair = createBlock("cypress_log_bridge_stair", () -> new Iron_Stair(AbstractBlock.Properties.of(Material.WOOD).noOcclusion().strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+	public static final RegistryObject<Block> cypress_bridge_pier = createBlock("cypress_bridge_pier", () -> new Support_Pillar(AbstractBlock.Properties.of(Material.WOOD).noOcclusion().strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+	public static final RegistryObject<Block> rope_cypress_bridge = createBlock("rope_cypress_bridge", () -> new Rope_Bridge(AbstractBlock.Properties.of(Material.WOOD).noOcclusion().strength(0.5F, 2.5F).sound(SoundType.WOOD)));
+
+	public static RegistryObject<Block> createBlock(String name, Supplier<? extends Block> supplier)
+    {
+        RegistryObject<Block> block = BLOCKS_REGISTRY.register(name, supplier);
+        MBBBItems.ITEMS_REGISTRY.register(name, () -> new BlockItem(block.get(), new Item.Properties().tab(McwBridgesBayouBlues.CMT)));
+        return block;
+    }
+}
