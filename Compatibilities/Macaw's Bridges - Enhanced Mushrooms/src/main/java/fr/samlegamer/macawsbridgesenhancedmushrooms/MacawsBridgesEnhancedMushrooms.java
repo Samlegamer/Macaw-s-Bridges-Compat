b562eba910e4189cb0fd8f0f2f7e27eef0ec1674
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2022-02-19 01:42:33 +0100

    Add files via upload

---
+++ Compatibilities/Macaw's Bridges - Enhanced Mushrooms/src/main/java/fr/samlegamer/macawsbridgesenhancedmushrooms/MacawsBridgesEnhancedMushrooms.java
@@ -0,0 +1,57 @@
+package fr.samlegamer.macawsbridgesenhancedmushrooms;
+
+import fr.samlegamer.macawsbridgesenhancedmushrooms.block.MBEMBlockRegistry;
+import fr.samlegamer.macawsbridgesenhancedmushrooms.item.MBEMItemRegistry;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+//import net.minecraft.client.renderer.RenderType;
+//import net.minecraft.client.renderer.RenderTypeLookup;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(value = MacawsBridgesEnhancedMushrooms.MODID)
+@Mod.EventBusSubscriber(modid = MacawsBridgesEnhancedMushrooms.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class MacawsBridgesEnhancedMushrooms
+{
+	public static final String MODID = "macawsbridgesenhancedmushrooms";
+	public static final Tab CMT = new Tab("mbenhancedmush_tab");
+
+	public MacawsBridgesEnhancedMushrooms()
+	{
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::ClientSetup);
+				
+		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
+		MBEMItemRegistry.ITEMS_REGISTRY.register(bus);
+		MBEMBlockRegistry.BLOCKS_REGISTRY.register(bus);
+	}
+	
+	private void setup(FMLCommonSetupEvent event) {}
+	
+	private void ClientSetup(FMLClientSetupEvent event)
+	{
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.brown_mushroom_log_bridge_middle.get(), RenderType.cutout());
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.red_mushroom_log_bridge_middle.get(), RenderType.cutout());
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.rope_brown_mushroom_bridge.get(), RenderType.cutout());
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.rope_red_mushroom_bridge.get(), RenderType.cutout());
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.brown_mushroom_bridge_pier.get(), RenderType.cutout());
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.red_mushroom_bridge_pier.get(), RenderType.cutout());
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.brown_mushroom_log_bridge_stair.get(), RenderType.cutout());
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.red_mushroom_log_bridge_stair.get(), RenderType.cutout());
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.brown_mushroom_rail_bridge.get(), RenderType.cutout());
+	 	RenderTypeLookup.setRenderLayer(MBEMBlockRegistry.red_mushroom_rail_bridge.get(), RenderType.cutout());
+	}
+	
+	static class Tab extends ItemGroup
+	{
+		public Tab(String label) { super(label); }
+		
+		@Override
+		public ItemStack makeIcon() { return new ItemStack(MBEMBlockRegistry.red_mushroom_log_bridge_middle.get()); }
+	}
+}
---
+++ Compatibilities/Macaw's Bridges - Shroomed/src/main/java/fr/samlegamer/mcwbridgesshroomed/item/MBSItemsRegistry.java
@@ -0,0 +1,11 @@
+package fr.samlegamer.mcwbridgesshroomed.item;
+
+import fr.samlegamer.mcwbridgesshroomed.McwBridgesShroomed;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+public class MBSItemsRegistry
+{
+	public static final DeferredRegister<Item> ITEMS_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, McwBridgesShroomed.MODID);
+}
