27e67b85a6a7142e2fbc88e3df81e03714c3f61a
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2022-04-16 22:50:43 +0200

    byg 1.1 fabric

---
+++ Compatibilities/Fabric/Macaw's Bridges - BYG/src/main/java/fr/samlegamer/macawsbridgesbyg/MacawsBridgesBYG.java
@@ -0,0 +1,19 @@
+package fr.samlegamer.macawsbridgesbyg;
+
+import fr.samlegamer.macawsbridgesbyg.block.MBBYGBlocksRegistry;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+public class MacawsBridgesBYG implements ModInitializer
+{
+	public static final ItemGroup TAB = FabricItemGroupBuilder.build(new Identifier("macawsbridgesbyg_tab"), () -> new ItemStack(MBBYGBlocksRegistry.aspen_log_bridge_middle));
+	
+	@Override
+	public void onInitialize()
+	{
+		MBBYGBlocksRegistry.registry();
+	}
+}
