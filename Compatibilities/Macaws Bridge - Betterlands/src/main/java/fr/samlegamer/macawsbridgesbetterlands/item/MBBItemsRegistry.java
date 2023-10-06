dd92f5e633249878fc4e2bd6eeb4c0e6c02dfc3f
Samlegamer <83614614+Samlegamer@users.noreply.github.com>
2023-10-06 12:00:27 +0200

    Delete Compatibilities directory

--- Compatibilities/Macaws Bridge - Betterlands/src/main/java/fr/samlegamer/macawsbridgesbetterlands/item/MBBItemsRegistry.java
+++
@@ -1,9 +0,0 @@
-package fr.samlegamer.macawsbridgesbetterlands.item;
-
-import fr.samlegamer.macawsbridgesbetterlands.MacawsBridgesBetterlands;
-import net.minecraft.item.Item;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
-
-public class MBBItemsRegistry
-{public static final DeferredRegister<Item> ITEMS_REGISTRY = DeferredRegister.create(ForgeRegistries.ITEMS, MacawsBridgesBetterlands.MODID);}
