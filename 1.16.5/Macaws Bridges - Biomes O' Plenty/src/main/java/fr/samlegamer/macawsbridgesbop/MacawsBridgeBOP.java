a0828b501a8b3f5f43c47f81ca9f9d920549e6c5
Samlegamer <samlegamer.yt@gmail.com>
2024-08-26 13:37:15 +0200

    testing new addons lib

--- 1.16.5/Macaws Bridges - Biomes O' Plenty/src/main/java/fr/samlegamer/macawsbridgesbop/MacawsBridgeBOP.java
+++ 1.16.5/Macaws Bridges - Biomes O' Plenty/src/main/java/fr/samlegamer/macawsbridgesbop/MacawsBridgeBOP.java
@@ -1,95 +1,34 @@
 package fr.samlegamer.macawsbridgesbop;
 
-import fr.samlegamer.macawsbridgesbop.blocks.MBBOPBlocksRegistry;
-import net.minecraft.client.renderer.RenderType;
-import net.minecraft.client.renderer.RenderTypeLookup;
-import net.minecraftforge.eventbus.api.IEventBus;
+import java.util.List;
+import fr.samlegamer.addonslib.Registration;
+import fr.samlegamer.addonslib.bridges.Bridges;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
 
 @Mod(value = MacawsBridgeBOP.MODID)
-@Mod.EventBusSubscriber(modid = MacawsBridgeBOP.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MacawsBridgeBOP
 {
 	public static final String MODID = "macawsbridgesbop";
-		
+	public static final ItemGroup MBBOP_TAB = new ItemGroup(MODID + ".tab") {
+	    @Override
+	    public ItemStack makeIcon() {
+	        return new ItemStack(Bridges.getBridgeBlocks().get(0).get());
+	    }
+	};
+	
 	public MacawsBridgeBOP()
 	{
-		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::ClientSetup);
-		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		MBBOPBlocksRegistry.BLOCKS_REGISTRY.register(bus);
-		MBBOPBlocksRegistry.ITEMS_REGISTRY.register(bus);
+		List<String> MAT_WOOD = List.of("cherry", "dead", "fir", "hellbark", "jacaranda", "magic", "mahogany", "palm", "redwood", "umbran", "willow");
+		DeferredRegister<Block> blockReg = Registration.blocks(MODID);
+		DeferredRegister<Item> itemReg = Registration.items(MODID);
+		Registration.init(blockReg, itemReg);
+		Bridges.setRegistrationWood(MAT_WOOD, blockReg, itemReg, MBBOP_TAB);
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(Bridges::setupClient);
 	}
-	
-	private void ClientSetup(FMLClientSetupEvent event)
-	{
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.cherry_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.dead_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.fir_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.hellbark_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.jacaranda_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.magic_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.mahogany_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.palm_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.redwood_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.umbran_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.willow_log_bridge_middle.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.cherry_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.dead_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.fir_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.hellbark_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.jacaranda_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.magic_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.mahogany_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.palm_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.redwood_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.umbran_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.willow_bridge_pier.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.cherry_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.dead_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.fir_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.hellbark_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.jacaranda_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.magic_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.mahogany_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.palm_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.redwood_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.umbran_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.willow_rail_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_cherry_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_dead_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_fir_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_hellbark_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_jacaranda_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_magic_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_mahogany_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_palm_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_redwood_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_umbran_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.rope_willow_bridge.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.cherry_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.dead_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.fir_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.hellbark_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.jacaranda_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.magic_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.mahogany_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.palm_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.redwood_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.umbran_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.willow_log_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.cherry_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.dead_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.fir_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.hellbark_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.jacaranda_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.magic_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.mahogany_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.palm_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.redwood_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.umbran_rope_bridge_stair.get(), RenderType.cutout());
-		RenderTypeLookup.setRenderLayer(MBBOPBlocksRegistry.willow_rope_bridge_stair.get(), RenderType.cutout());
-
-	}	
 }
