05f6d46355667d8a4f87d4d3925b0b6926158478
Samlegamer <samlegamer.yt@gmail.com>
2024-10-10 20:51:25 +0200

    rope bridge loot table

--- 1.16.5/Macaws Bridges - Biomes O' Plenty/src/main/java/fr/samlegamer/macawsbridgesbop/MacawsBridgeBOP.java
+++ 1.16.5/Macaws Bridges - Biomes O' Plenty/src/main/java/fr/samlegamer/macawsbridgesbop/MacawsBridgeBOP.java
@@ -1,12 +1,14 @@
 package fr.samlegamer.macawsbridgesbop;
 
 import java.util.List;
+import java.util.Arrays;
 import fr.samlegamer.addonslib.Registration;
 import fr.samlegamer.addonslib.bridges.Bridges;
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,17 +20,21 @@ public class MacawsBridgeBOP
 	public static final ItemGroup MBBOP_TAB = new ItemGroup(MODID + ".tab") {
 	    @Override
 	    public ItemStack makeIcon() {
-	        return new ItemStack(Bridges.getBridgeBlocks().get(0).get());
+	        return new ItemStack(LOGO.get());
 	    }
 	};
+	private static final DeferredRegister<Block> blockReg = Registration.blocks(MODID);
+	private static final DeferredRegister<Item> itemReg = Registration.items(MODID);
+
+	protected static final RegistryObject<Item> LOGO = itemReg.register("logo", ()-> new Item(new Item.Properties()));
 	
 	public MacawsBridgeBOP()
 	{
-		List<String> MAT_WOOD = List.of("cherry", "dead", "fir", "hellbark", "jacaranda", "magic", "mahogany", "palm", "redwood", "umbran", "willow");
-		DeferredRegister<Block> blockReg = Registration.blocks(MODID);
-		DeferredRegister<Item> itemReg = Registration.items(MODID);
+		Bridges.getBridgeBlocks().clear();
+		List<String> MAT_WOOD = Arrays.asList("cherry", "dead", "fir", "hellbark", "jacaranda", "magic", "mahogany", "palm", "redwood", "umbran", "willow");
 		Registration.init(blockReg, itemReg);
 		Bridges.setRegistrationWood(MAT_WOOD, blockReg, itemReg, MBBOP_TAB);
 		FMLJavaModLoadingContext.get().getModEventBus().addListener(Bridges::setupClient);
+		
 	}
 }
